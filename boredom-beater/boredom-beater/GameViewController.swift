dee8db551f80edae6f2e8837df2332b7fa3fefe1
zlarsen <zlarsen@dmail.dixie.edu>
2016-06-06 14:09:20 -0600

    adds app icon, images, and increases dificulty

--- boredom-beater/boredom-beater/GameViewController.swift
+++ boredom-beater/boredom-beater/GameViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 import AdSupport
 
 let gameLength = 10.0
+let diffLevels: [Int] = [10,25,50,75,100,125,150,175,200,225,250,275,300,325,350]
+let decreaseValues: [Double] = [0.11,0.13,0.15,0.17,0.19,0.21,0.23,0.25,0.27,0.29,0.31,0.33,0.35,0.37,0.39]
 
 class GameViewController: UIViewController, FlurryAdBannerDelegate {
     
@@ -20,7 +22,9 @@ class GameViewController: UIViewController, FlurryAdBannerDelegate {
     var green = UIButton()
     var restartGameButton = UIButton()
     var newHiScoreLabel = UILabel()
+    var hiScoreLabel = UILabel()
     var gameOverView =  UIView()
+    var tombstone = UIImageView()
     var gameInPlay = false
     var clickCounter = 0
     var firstTimeClicked = true
@@ -28,6 +32,7 @@ class GameViewController: UIViewController, FlurryAdBannerDelegate {
     let savedScores = NSUserDefaults.standardUserDefaults()
     var hiscore = 0
     var newHiScore = false
+    var counterDec = 0.1
     
     
     let screenSize: CGRect = UIScreen.mainScreen().bounds
@@ -40,12 +45,12 @@ class GameViewController: UIViewController, FlurryAdBannerDelegate {
         
         green.hidden = true
         green.frame = CGRect(x: self.view.bounds.width/2-green.bounds.width, y: green.bounds.height, width: 100, height: 100)
-        green.setTitle("+5 Sec", forState: .Normal)
+        green.setBackgroundImage(UIImage(named: "green-button.png"), forState: .Normal)
         green.setTitleColor(UIColor.greenColor(), forState: .Normal)
         green.addTarget(self, action: #selector(GameViewController.greenButtonAction(_:)), forControlEvents: UIControlEvents.TouchUpInside)
         
         press.frame = CGRect(x: self.view.bounds.width/2-50, y: self.view.bounds.height/2-50, width: 100, height: 100)
-        press.setTitle("Punch", forState: .Normal)
+        press.setBackgroundImage(UIImage(named: "red-button.png"), forState: .Normal)
         press.setTitleColor(UIColor.redColor(), forState: .Normal)
         press.addTarget(self, action: #selector(GameViewController.pressButtonAction(_:)), forControlEvents: UIControlEvents.TouchUpInside)
         
@@ -62,14 +67,23 @@ class GameViewController: UIViewController, FlurryAdBannerDelegate {
         restartGameButton.addTarget(self, action: #selector(GameViewController.restartGameButtonAction(_:)), forControlEvents: UIControlEvents.TouchUpInside)
         
         newHiScoreLabel.hidden = true
-        newHiScoreLabel.textColor = UIColor.blueColor()
-        newHiScoreLabel.frame = CGRect(x: gameOverView.frame.width/2-90, y: gameOverView.frame.height/2-65, width: 180, height: 30)
-
+        newHiScoreLabel.textColor = UIColor.redColor()
+        newHiScoreLabel.adjustsFontSizeToFitWidth = true
+        score.adjustsFontSizeToFitWidth = true
+        newHiScoreLabel.frame = CGRect(x: gameOverView.frame.width/2-90, y: gameOverView.frame.height/2-95, width: 180, height: 30)
         
+        hiScoreLabel.textColor = UIColor.blueColor()
+        hiScoreLabel.adjustsFontSizeToFitWidth = true
+        hiScoreLabel.frame = CGRect(x: gameOverView.frame.width/2-40, y: gameOverView.frame.height/2-65, width: 80, height: 30)
+
+        tombstone.frame = CGRect(x: gameOverView.frame.width/2-180, y: gameOverView.frame.height/2+20, width: 400, height: 200)
+        tombstone.image = UIImage(named: "tombstone.png")
         
         self.view.addSubview(gameOverView)
-        gameOverView.addSubview(restartGameButton)
         gameOverView.addSubview(newHiScoreLabel)
+        gameOverView.addSubview(hiScoreLabel)
+        gameOverView.addSubview(restartGameButton)
+        gameOverView.addSubview(tombstone)
         
         hiscore = initializeCounters("hiscore")
     }
@@ -90,16 +104,18 @@ class GameViewController: UIViewController, FlurryAdBannerDelegate {
     }
     
     func startGame() {
-        print("Game has started")
         timer.invalidate()
         timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target:self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
         gameInPlay = true
     }
     
     func updateCounter() {
-        counter -= 0.1
+        if (diffLevels.contains(clickCounter)) {
+            counterDec = decreaseValues[clickCounter]
+        }
+        counter -= counterDec
         counter = Double(round(10*counter)/10)
-        if (counter == 0.0) {
+        if (counter <= 0.0) {
             timer.invalidate()
             gameOver()
         }
@@ -107,7 +123,6 @@ class GameViewController: UIViewController, FlurryAdBannerDelegate {
     }
     
     func gameOver() {
-        print("game over!")
         gameInPlay = false
         press.hidden = true
         green.hidden = true
@@ -119,6 +134,7 @@ class GameViewController: UIViewController, FlurryAdBannerDelegate {
             savedScores.setValue(clickCounter, forKey: "hiscore")
             hiscore = initializeCounters("hiscore")
         }
+        hiScoreLabel.text = "Hiscore: \(hiscore)"
     }
     
     func gameAction() {
@@ -139,12 +155,14 @@ class GameViewController: UIViewController, FlurryAdBannerDelegate {
     func restartGame() {
         gameOverView.hidden = true
         press.hidden = false
-        gameInPlay = true
         counter = gameLength
         clickCounter = 0
         newHiScoreLabel.hidden = true
         score.text = "Score 0"
-        startGame()
+        timerLabel.text = "Time 10.0"
+        counterDec = 0.1
+        press.frame.origin = CGPoint(x: self.view.bounds.width/2-50, y: self.view.bounds.height/2-50)
+        firstTimeClicked = true
     }
     
     func pressButtonAction(sender:UIButton!) {
@@ -178,11 +196,9 @@ class GameViewController: UIViewController, FlurryAdBannerDelegate {
     
     func adBannerDidFetchAd(bannerAd: FlurryAdBanner!) {
         // Received Ad
-        print("adFetched")
         adBanner.displayAdInView(self.gameOverView, viewControllerForPresentation: self)
     }
     func adBannerDidRender(bannerAd: FlurryAdBanner!) {
-        print("ad rendered")
         // For when your ad renders. Nice to check to make sure your ad is working
     }
 }
